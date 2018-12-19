860a2f07d713538ff629f0031fa7160bbe917b56
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-19 17:13:30 +0100

    Fix require statement for twitter-bootstrap-components-rails.

--- lib/ecm_files.rb
+++ lib/ecm_files.rb
@@ -1,6 +1,6 @@
 require 'route_translator'
 require 'rails-add_ons'
-require 'twitter_bootstrap_components_rails'
+require 'twitter-bootstrap-components-rails'
 
 require 'ecm/files/configuration'
 require "ecm/files/engine"
